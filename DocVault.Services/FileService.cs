f88faa0747f44282681caf63a112f32280ad7daa
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2021-12-21 09:39:17 -0600

    Removed unused JSON functions from FileService

--- DocVault.Services/FileService.cs
+++ DocVault.Services/FileService.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using System.Security.Cryptography;
-using Newtonsoft.Json;
 
 namespace DocVault.Services
 {
@@ -12,30 +11,6 @@ namespace DocVault.Services
             using FileStream stream = File.OpenRead(fileName);
 
             return md5.ComputeHash(stream);
-        }
-
-        public static void SaveFormattedJson(string filename, string jsonText)
-        {
-            File.WriteAllText(filename, JsonPrettify(jsonText));
-        }
-
-        private static string JsonPrettify(string json)
-        {
-            using StringReader stringReader = new StringReader(json);
-            using StringWriter stringWriter = new StringWriter();
-
-            JsonTextReader jsonReader =
-                new JsonTextReader(stringReader);
-
-            JsonTextWriter jsonWriter =
-                new JsonTextWriter(stringWriter)
-                {
-                    Formatting = Formatting.Indented
-                };
-
-            jsonWriter.WriteToken(jsonReader);
-
-            return stringWriter.ToString();
         }
     }
 }
